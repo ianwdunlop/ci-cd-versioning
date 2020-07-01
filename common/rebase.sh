1445d4a1411c95cea4fbd135962c6dff37f41c77
Mark Laing <mark.laing@md.catapult.org.uk>
2020-07-01 08:32:02 +0100

    Handles errors differently in rebase script.

--- common/rebase.sh
+++ common/rebase.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+function ignoreError() {
+    if [ "$1" -ne 0 ]; then
+        exit 0
+    fi
+}
+
 if [ -z "$REBASE_BRANCH" ]; then
     echo "No rebase branch configured. Skipping..." >&2
     exit 0
@@ -8,30 +14,18 @@ fi
 eval "$(ssh-agent -s)"
 echo "${SSH_PRIVATE_KEY}" | ssh-add -
 
-if git checkout "$REBASE_BRANCH" &> /dev/null; then
-    echo "Checked out $REBASE_BRANCH..."
-else 
-    echo "Warning, rebase failed. Couldn't check out $REBASE_BRANCH. Exiting..." >&2
-    exit 0
-fi
+echo "Checking out $REBASE_BRANCH..."
+git checkout "$REBASE_BRANCH"
+ignoreError $?
 
-if git pull &> /dev/null; then
-    echo "Pulled changes to $REBASE_BRANCH..."
-else 
-    echo "Warning, rebase failed. Couldn't pull changes to $REBASE_BRANCH. Exiting..." >&2
-    exit 0
-fi
+echo "Pulling changes to $REBASE_BRANCH..."
+git pull
+ignoreError $?
 
-if git rebase "$CI_COMMIT_BRANCH" &> /dev/null; then
-    echo "Rebased $REBASE_BRANCH onto $CI_COMMIT_BRANCH..."
-else 
-    echo "Warning, rebase failed. Couldn't rebase $REBASE_BRANCH onto $CI_COMMIT_BRANCH. Exiting..." >&2
-    exit 0
-fi
+echo "Rebasing $REBASE_BRANCH onto $CI_COMMIT_BRANCH..."
+git rebase "$CI_COMMIT_BRANCH"
+ignoreError $?
 
-if git push origin "$REBASE_BRANCH" &> /dev/null; then 
-    echo "Pushed changes successfully"
-else 
-    echo "Warning, rebase failed. Couldn't push changes to $REBASE_BRANCH. Exiting..." >&2
-    exit 0
-fi
+echo "Pushing changes to $REBASE_BRANCH..."
+git push origin "$REBASE_BRANCH"
+ignoreError $?
