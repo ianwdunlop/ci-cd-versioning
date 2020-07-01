f39181a78b345e15aae272084518e2ea32b3b3da
Mark Laing <mark.laing@md.catapult.org.uk>
2020-07-01 08:03:00 +0100

    Handles error from npm version subcommand.

--- node/version.sh
+++ node/version.sh
@@ -4,6 +4,8 @@ eval "$(ssh-agent -s)"
 echo "${SSH_PRIVATE_KEY}" | ssh-add -
 
 RELEASE_TAG=$(npm version "$BUMP" -m "Setting version to v%s")
+reportError $?
+
 git push origin "$CI_COMMIT_BRANCH"
 git push origin --tags
 
