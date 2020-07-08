fe831119fa944842426cbf869e07c50ecd055198
Mark Laing <mark.laing@md.catapult.org.uk>
2020-07-08 13:00:39 +0100

    Adds functionality to set up git for go get operations against private packages.

---
+++ golang/setup-go-private.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo -e "machine gitlab.mdcatapult.io\n\tlogin $GIT_RELEASE_USER\n\tpassword $GIT_RELEASE_TOKEN" > ~/.netrc
+GOPRIVATE="$CI_SERVER_HOST:$CI_SERVER_PORT/*"
+command -v ssh-agent || ( apt-get install -qq openssh-client )
+eval "$(ssh-agent -s)"
+echo "${SSH_PRIVATE_KEY}" | ssh-add -
+mkdir -p ~/.ssh
+[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+
+export GOPRIVATE
