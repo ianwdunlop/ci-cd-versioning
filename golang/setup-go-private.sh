d61d686f173a321b3cf397b87cf2fc9b6764a025
Mark Laing <mark.laing@md.catapult.org.uk>
2021-03-29 16:21:12 +0100

    Remove readonly token and use ci job token for go get.

--- golang/setup-go-private.sh
+++ golang/setup-go-private.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck source=.
 . "$DIR/utils.sh"
 
-echo -e "machine $CI_SERVER_HOST\n\tlogin $CI_READONLY_USER\n\tpassword $CI_READONLY_TOKEN" > ~/.netrc
+echo -e "machine $CI_SERVER_HOST\n\tlogin gitlab-ci-token\n\tpassword $CI_JOB_TOKEN" > ~/.netrc
 reportError $?
 GOPRIVATE="$CI_SERVER_HOST:$CI_SERVER_PORT/*"
 
