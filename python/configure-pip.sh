429704ef157b3181da8486a7897c85b9436ae0ea
Mark Laing <mark.laing@md.catapult.org.uk>
2021-04-24 20:15:58 +0100

    Fix configure pip script for new nexus host.

--- python/configure-pip.sh
+++ python/configure-pip.sh
@@ -3,11 +3,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck source=.
 . "$DIR/utils.sh"
 
-pip config set global.trusted-host nexus.mdcatapult.io
+pip config set global.trusted-host "nexus.wopr.inf.mdc pypi.org files.pythonhosted.org"
 reportError $?
 
-pip config set global.index "https://$NEXUS_USERNAME:$NEXUS_PASSWORD@nexus.mdcatapult.io/repository/pypi-all/pypi"
+pip config set global.index "https://$NEXUS_USERNAME:$NEXUS_PASSWORD@nexus.wopr.inf.mdc/repository/pypi-all/pypi"
 reportError $?
 
-pip config set global.index-url "https://$NEXUS_USERNAME:$NEXUS_PASSWORD@nexus.mdcatapult.io/repository/pypi-all/simple"
+pip config set global.index-url "https://$NEXUS_USERNAME:$NEXUS_PASSWORD@nexus.wopr.inf.mdc/repository/pypi-all/simple"
 reportError $?
